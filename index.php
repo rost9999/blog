41bb6b5e3431d779deca060995812ae6f095f476
Rost <wr2st12@gmail.com>
2021-07-26 16:20:53 +0300

    work with blog

--- index.php
+++ index.php
@@ -4,5 +4,24 @@ require_once "./vendor/autoload.php";
 
 use controllers\HomeController;
 
-$controller = new HomeController;
-$controller->default();
+$url = explode('/', $_SERVER['REQUEST_URI']);
+$controllerName = $url[1];
+$method = $url[2];
+
+if (empty($url[1])) {
+    $controllerName = HomeController::class;
+} else {
+    $controllerName = "controllers\\" . $controllerName . "Controller";
+}
+
+if (empty($method)) {
+    $method = 'default';
+}
+
+if (method_exists($controllerName, $method)) {
+    $controller = new $controllerName();
+    $controller->$method();
+} else {
+    http_response_code(404);
+    echo 404;
+}
