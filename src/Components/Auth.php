c3987b773f3893d83bdb7e10a032a711c79dc4a2
Rost <wr2st12@gmail.com>
2021-08-13 10:34:41 +0300

    change Auth

--- src/Components/Auth.php
+++ src/Components/Auth.php
@@ -10,15 +10,12 @@ class Auth
 
     public static function user()
     {
-        if (!self::$user) {
-            if (!empty($_SESSION['id'])) {
+        if (!self::$user && isset($_SESSION['id'])) {
                 $pdo = DbConnection::getInstance();
                 $sql = $pdo->prepare("SELECT * FROM `users` WHERE id = :id LIMIT 1");
                 $sql->execute(['id' => $_SESSION['id']]);
-                return $sql->fetch(PDO::FETCH_ASSOC);
-            } else {
-                return null;
-            }
-        } else self::$user;
+                self::$user = $sql->fetch(PDO::FETCH_ASSOC);
+        }
+        return self::$user;
     }
 }
